040088ac83e548a1daed39f28b7bd7ad5e340a1e
Dario Semino <orbislibroscontacto@gmail.com>
2024-05-07 10:39:28 -0300

    with articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
       title="Cincuenta mil tazas de café"
       description="Literatura, deudas, amores y absoluto en Balzac – La novela de su vida de Stefan Zweig"
       mainImage={vautrin}
-      link=""
+      link="cincuenta-mil-tazas-de-cafe"
       
       />
       <Card
@@ -65,13 +65,13 @@ export default function Home() {
       title="Las aventuras de una idea"
       description="Un viaje a través de la concepción occidental del universo, desde Platón hasta el siglo XIX, en La Gran Cadena del Ser de Arthur O. Lovejoy"
       mainImage={idea}
-      link="articulos/2"
+      link="las-aventuras-de-una-idea"
 
       />
       <Card
       cardType={styles.art2}
       title="Cinabrio, psicoducto, misterio"
-      description="Una ciudad devorada por la selva, una tumba olvidada y el paso de los siglos en La Reina Roja, una tumba real de Arnoldo González Cruz"
+      description="Una ciudad devorada por la selva en La Reina Roja, una tumba real de Arnoldo González Cruz"
       mainImage={reina}
       link="articulos/2"
 
@@ -112,6 +112,10 @@ export default function Home() {
      <Footer
      mainImage={guarda}
      />
+
+     <div>
+     <h2>Bibliografía</h2><ol> <li><p>La edición original en inglés realizada por Harvard University Press está en internet. Es la versión que utilicé para el artículo. Se puede ver aquí:</p><a href='https://ia600704.us.archive.org/25/items/ArthurO.LovejoyTheGreatChainOfBeing/Arthur%20O.%20Lovejoy%20-%20The%20Great%20Chain%20of%20Being.pdf'>archive.org</a></li><li><p>Aquí se puede ver un resumen más extenso del libro:</p><a href='https://entenderelmundocom.files.wordpress.com/2018/04/lagrancadenadelser_lovejoy-v2.pdf'>entenderelmundo.com</a></li><li><p>Aquí una reseña más reciente:</p><a href='Book Review and Reflection: Arthur Lovejoy’s “The Great Chain of Being”'>Book Review and Reflection: Arthur Lovejoy’s “The Great Chain of Being”</a></li><li><p>Aquí se puede ver un artículo sobre las críticas que recibió el libro después de su publicación: <i>Lovejoy’s The Great Chain of Being after Fifty Years, Daniel J. Wilson, Journal of the History of Ideas, Vol. 48, No. 2 (Apr. – Jun., 1987), pp. 187-206 (20 pages)</i>. Se puede descargar en PDF aquí:</p> <a href='https://www.jstor.org/stable/2709553?seq=1'>jston.org</a> </li><li><p>Aquí unos artículos que hablan de la continuidad de la Cadena del Ser en el evolucionismo actual:</p><a href='https://www.nature.com/articles/435429a'>nature.com</a> <br /><a href='https://evolution-outreach.biomedcentral.com/articles/10.1186/1936-6434-6-18'>evolution-outreach.biomedcentral.com</a></li><li><p>Aquí se puede descargar un libro entero dedicado principalmente al problema del principio de plenitud en Santo Tomás, de la autora venezolana María Guadalupe Llanes: <br /> <a href='https://www.unimet.edu.ve/wp-content/uploads/2019/10/El-Principio-de-plenitud-en-el-pensamiento-antiguo-y-de-la-Edad-Media.pdf'>unimet.edu.ve</a></p></li><li></li><li><p>Con respecto al Timeo copio abajo el párrafo que ilustra a la perfección la lectura que realiza Lovejoy. En el texto original la palabra utilizada es agathos, la cual significa “bueno” y funciona como su equivalente en castellano para referir lo bueno en todos sus sentidos. En la última línea, cuando se habla del “óptimo”, la palabra en el original es aristos, la cual funciona como un superlativo de agathos. </p><p>“<i>Resumiendo, los primeros filósofos son los que instauran un plano de inmanencia como un tamiz tendido sobre el caos. Se oponen en este sentido a los Sabios, que son personajes de la religión, sacerdotes, porque conciben la instauración de un orden siempre trascendente, impuesto desde fuera por un gran déspota o por un dios superior a los demás, a imagen de Eris, tras guerras que superan cualquier agón y odios que recusan de antemano los desafíos de la rivalidad.’ Hay religión cada vez que hay trascendencia, Ser vertical, Estado imperial en el cíelo o en la tierra, y hay Filosofía cada vez que hay inmanencia, aun cuando sirva de ruedo al agón y a la rivalidad.</i>” ¿Qué es la filosofía? Gilles Deleuze y Félix Guattari, Ed. Anagrama p. 47</p></li> <li> <p>La cita final corresponde al libro: <i> Los evangelios judíos, la historia del Cristo judío</i> de Daniel Boyarin, editado por Lilmod y Prometeo Libros, 2016. p. 54.</p></li><li><p>Aquí agrego los links de la página y del blog del Journal of History of Ideas fundado inicialmente por Lovejoy en 1940 y que todavía sigue en actividad:</p><br /><a href='https://jhi.pennpress.org/home/'>jhi.pennpress.org</a><br /><a href='https://jhiblog.org'>jhiblog.org</a></li><li><p>La imagen inicial está tomada del libro <i>Rhetorica Christiana</i>, realizado por Fray Diego Valadés, publicado en 1579. Fray Diego Valadés fue un misionero franciscano que ofició de historiador, lingüista, grabador y políglota. Nacio en Nueva España, hijo de un conquistador y de una mujer tlaxcalteca. Fue el primer mexicano en publicar un libro en España.</p></li> </ol> </div>
+
       </main>
   );
 }
