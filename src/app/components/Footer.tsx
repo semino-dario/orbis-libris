b40d00502fc8c4480590aa3d1a52308c151f4185
Darío Semino <dariomartinsemino@gmail.com>
2024-12-27 10:47:13 -0300

    adjust site name size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from '../page.module.scss'
 import { StaticImageData } from 'next/image'; 
+import  ornament  from './images/footer-ornament.svg'
 
 interface FooterProps {
     mainImage: string | StaticImageData,
@@ -15,7 +16,9 @@ const Footer: React.FC<FooterProps> = ({ mainImage }) => {
 <div className={styles.footer}>
      <img className={styles.closeImage} src={imageSource} alt="" />
      <p className={styles.footerText}>
-     Orbis Libris es un sitio creado y administrado por Darío Semino. Comenzó a estar en línea en el mes de abril de MMXXI, segundo año de la Gran Pandemia que asoló el orbe y le recordó a los frágiles humanos el consuelo de los libros.     </p>
+     Orbis Libris es un sitio creado y administrado por Darío Semino. Comenzó a estar en línea en el mes de abril de MMXXI, segundo año de la Gran Pandemia que asoló el orbe y le recordó a los frágiles humanos el consuelo de los libros. 
+     </p>
+     <img className={styles.ornament}src={ornament.src} alt="" />
      </div>)}
 
 export default Footer
