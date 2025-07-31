af79e7658556ec465cbb057abb9a1fbb426f7a47
Darío Semino <dariomartinsemino@gmail.com>
2025-07-31 10:27:31 +0200

    eliminate unnecesary div in article

--- src/app/articulos/[id]/page.tsx
+++ src/app/articulos/[id]/page.tsx
@@ -1,30 +1,25 @@
-import Footer from "@/app/components/Footer";
 import Article from "../../components/Article";
-import content from '../../content.json'
+import content from "../../content.json";
 type ContentKey = keyof typeof content;
 
+export default function Articulo({ params }: { params: { id: string } }) {
+  const datafetch = params.id as ContentKey;
+  const data = content[datafetch].content;
 
-export default function Articulo ({params}: {params: {id: string }}) {
+  const image = data.image;
+  const date = data.date;
+  const title = data.title;
+  const description = data.description;
+  const mainContent = data.text;
 
- const datafetch = params.id as ContentKey
- const data = content[datafetch].content
-
-const image = data.image
-   const date = data.date
-   const  title = data.title
-   const description = data.description
-   const mainContent = data.text
-
-    return(
-        <div>
-            <Article
-            image={image}
-            date={date}
-            title={title}
-            description={description}
-            mainContent={mainContent}
-            url={`https://orbislibris.com/articulos/${params.id}`}     
-            />
-        </div>
-    )
+  return (
+    <Article
+      image={image}
+      date={date}
+      title={title}
+      description={description}
+      mainContent={mainContent}
+      url={`https://orbislibris.com/articulos/${params.id}`}
+    />
+  );
 }
