3ee70501615a6c7c055318d58862c0c3bdd36358
Darío Semino <dariomartinsemino@gmail.com>
2026-03-17 15:10:31 +0100

    add more images in lalibre

--- src/app/articulos/[id]/page.tsx
+++ src/app/articulos/[id]/page.tsx
@@ -57,29 +57,3 @@ export default function Articulo({ params }: { params: { id: string } }) {
     />
   );
 }
-
-// import Article from "../../components/Article";
-// import content from "../../content.json";
-// type ContentKey = keyof typeof content;
-
-// export default function Articulo({ params }: { params: { id: string } }) {
-//   const datafetch = params.id as ContentKey;
-//   const data = content[datafetch].content;
-
-//   const image = data.image;
-//   const date = data.date;
-//   const title = data.title;
-//   const description = data.description;
-//   const mainContent = data.text;
-
-//   return (
-//     <Article
-//       image={image}
-//       date={date}
-//       title={title}
-//       description={description}
-//       mainContent={mainContent}
-//       url={`https://orbislibris.com/articulos/${params.id}`}
-//     />
-//   );
-// }
